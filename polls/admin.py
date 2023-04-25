d903ca5fad0cd170175d19a6696d5d1a45969e5a
Bakdolot <genry03gh@gmail.com>
2023-04-26 00:07:36 +0600

    add some wrong cases in list_display

--- polls/admin.py
+++ polls/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, Some, SomeM2M
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ["pub_date", "question_text", "choice"]
+    list_display = ["choice", "choice_set", "somem2m", "SomeM2M_question+", "somem2m_set", "__module__", "__doc__", "objects"]
     
 
 class ChoiceAdmin(admin.ModelAdmin):
-    fields = ["question", "choice_text", "votes"]
+    pass
+    
+
+class SomeAdmin(admin.ModelAdmin):
+    pass
+    
+    
+class SomeM2MAdmin(admin.ModelAdmin):
+    pass
 
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Some, SomeAdmin)
+admin.site.register(SomeM2M, SomeM2MAdmin)
--- polls/models.py
+++ polls/models.py
@@ -11,3 +11,12 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class Some(models.Model):
+    question = models.OneToOneField(Question, on_delete=models.CASCADE, blank=True)
+    qwe = models.IntegerField()
+
+
+class SomeM2M(models.Model):
+    question = models.ManyToManyField(Question)
